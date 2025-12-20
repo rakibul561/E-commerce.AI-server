b04afef6a12461205002520389b41886e238055b
Rakibul Hasan <rakibul561@gmail.com>
2025-12-20 02:57:00 -0800

    otp verify successfully

--- src/app/middlewares/authRateLimiter.ts
+++ src/app/middlewares/authRateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const authRateLimiter = rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 30,
+    max: 5,
     message: {
         success: false,
         message: "Too many login attempts. Try again after 1 minutes",
